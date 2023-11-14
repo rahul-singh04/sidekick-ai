380810112a0407da480108b5fff1d5409beb372c
theOGCodeWitcher <rahulsingh97r@gmail.com>
2023-11-14 16:46:54 -0500

    navbar added

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div className="">
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div className=""></div>;
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import { Sparkles } from "lucide-react";
+
+// import { MobileSidebar } from "@/components/mobile-sidebar";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+// import { useProModal } from "@/hooks/use-pro-modal";
+
+const poppinsFont = Poppins({ weight: "600", subsets: ["latin"] });
+interface NavbarProps {
+  isPro: boolean;
+}
+
+export const Navbar = () => {
+  //   const proModal = useProModal();
+
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        {/* <MobileSidebar isPro={isPro} /> */}
+        <Link href="/">
+          <h1
+            className={`hidden md:block text-xl md:text-3xl font-bold text-primary  ${poppinsFont.className}`}
+          >
+            Sidekick.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        {/* {!isPro && ( */}
+        <Button
+          //   onClick={proModal.onOpen}
+          size="sm"
+          variant="premium"
+        >
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+        </Button>
+        {/* } */}
+        <ThemeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
